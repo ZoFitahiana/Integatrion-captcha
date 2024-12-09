a63103ed818c86d4ab9d497d29d636d38f12f526
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-12-09 09:58:41 +0100

    fix: error

--- src/app/component/Home.jsx
+++ src/app/component/Home.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Recaptcha from "./Recaptcha";
 
@@ -6,37 +6,36 @@ const Home = () => {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (window.grecaptcha) {
-            window.grecaptcha.ready(() => {
-                window.grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY, { action: 'submit' }).then((token) => {
-                    fetch('/api/verify-recaptcha', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ recaptchaToken: token }),
-                    })
-                        .then((response) => response.json())
-                        .then((data) => {
-                            if (data.success) {
-                                console.log('reCAPTCHA vérifié avec succès');
-                            } else {
-                                console.log('Échec de la vérification reCAPTCHA');
-                            }
-                        })
-                        .catch((error) => {
-                            console.error('Erreur lors de la vérification reCAPTCHA:', error);
-                        });
+        const recaptchaResponse = window.grecaptcha.getResponse();
+
+        if (recaptchaResponse) {
+            fetch('/api/verify-recaptcha', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ recaptchaToken: recaptchaResponse }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.success) {
+                        console.log('reCAPTCHA vérifié avec succès');
+                    } else {
+                        console.log('Échec de la vérification reCAPTCHA');
+                    }
+                })
+                .catch((error) => {
+                    console.error('Erreur lors de la vérification reCAPTCHA:', error);
                 });
-            });
         } else {
-            console.error('grecaptcha object is not available');
+            console.error('reCAPTCHA non validé');
         }
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <Recaptcha />
+            <div className="g-recaptcha" data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}></div>
             <button type="submit">Soumettre</button>
         </form>
     );
--- src/app/component/Recaptcha.jsx
+++ src/app/component/Recaptcha.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 const Recaptcha = () => {
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = `https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`;
+        script.src = `https://www.google.com/recaptcha/api.js`;
         script.async = true;
         script.defer = true;
         document.body.appendChild(script);
